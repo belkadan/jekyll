3b888a028abb55551e1bf401fa8c3a961bf15803
Jordy Rose <jediknil@belkadan.com>
2011-07-08 22:26:40 -0700

    Don't bother calculating site_payload just to get a list of posts for the paginator.

--- lib/jekyll/generators/pagination.rb
+++ lib/jekyll/generators/pagination.rb
@@ -30,7 +30,7 @@ module Jekyll
     #                   "previous_page" => <Number>,
     #                   "next_page" => <Number> }}
     def paginate(site, page)
-      all_posts = site.site_payload['site']['posts']
+      all_posts = site.posts.reverse
       pages = Pager.calculate_pages(all_posts, site.config['paginate'].to_i)
       (1..pages).each do |num_page|
         pager = Pager.new(site.config, num_page, all_posts, pages)
